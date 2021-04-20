13c618846a211ad7ba6a18662c9c70d92216b258
jramosss <jramostod@gmail.com>
2021-04-20 13:39:35 -0300

    fixed: endpoint was being rendered several times

--- App.js
+++ App.js
@@ -7,21 +7,17 @@ const phrase = domain + 'home'
 const App = () => {
   const [data, setData] = useState([]);
 
-  fetch(phrase)
-  .then((response) => response.json())
-  .then((json) => setData(json))
-  .catch((error) => console.error(error))
-  .finally(_ => console.log("Texttt: ",data.text, " Authorrr: ",data.author));
+  useEffect(() => {
+    fetch(phrase)
+    .then((response) => response.json())
+    .then((json) => setData(json))
+    .catch((error) => console.error(error))
+    .finally(_ => console.log("Texttt: ",data.text, " Authorrr: ",data.author));
+  },[]);
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      <FlatList
-        data={data}
-        keyExtractor={({ id }, index) => id}
-        renderItem={({ item }) => (
-          <Text>{item.text}, {item.author}</Text>
-        )}
-      />
+      <Text>{data.text}, {data.author}</Text>
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# *We should think of a name for this project* -Client
+
+Client side for our wonderful app
+
+## How to get this thing running?
+* npm i (if you haven`t done this before)
+* expo start / npm start
