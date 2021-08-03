7b0dbe3950ed981aef130972065cdbad28ae9f4e
jramosss <jramostod@gmail.com>
2021-08-03 15:00:05 -0300

    Blue background

--- components/Phrase.tsx
+++ components/Phrase.tsx
@@ -1,56 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import {
   Text,
   StyleSheet,
   Platform,
   ScrollView,
-  SafeAreaView
-} from 'react-native';
-import Database from '../db/db';
-import { Phrase } from '../utils/interfaces';
+  SafeAreaView,
+  StatusBar,
+} from 'react-native'
+import Database from '../db/db'
+import { Phrase } from '../utils/interfaces'
 
-const db = new Database();
+const db = new Database()
 //import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
 
 const emptyPhrase: Phrase = {
-  text: "",
-  author: "",
-  date: ""
+  text: '',
+  author: '',
+  date: '',
 }
 
 const styles = StyleSheet.create({
   phraseView: {
-    marginHorizontal: 10,
     flexGrow: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#024959',
+    width: '100%',
+    height: '100%',
+    paddingHorizontal: 25,
   },
   phrase: {
     textAlign: 'center',
     fontSize: 30,
     fontStyle: 'italic',
-    color: 'black',
+    color: 'white',
   },
   author: {
-    textAlign: 'right',
-    justifyContent: 'flex-end',
     alignSelf: 'flex-end',
     fontSize: 20,
-    fontStyle: 'italic'
-  }
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    color: 'white',
+  },
 })
 
 export default function PhraseView() {
-  const [data, setData] = useState(emptyPhrase);
+  const [data, setData] = useState(emptyPhrase)
   //TODO change the view when the user rotates the device
   //const { isRotated } = useDeviceOrientation();
-  useEffect(() =>
-    setData(db.getDailyPhrase())
-    , []);
+  useEffect(() => setData(db.getDailyPhrase()), [])
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      {/*This was made by mistake by i actually kinda like it */}
+      <StatusBar backgroundColor='#024959' />
       <ScrollView contentContainerStyle={styles.phraseView}>
-        <Text style={styles.phrase}>{data.text}</Text>
+        <Text style={styles.phrase}>
+          {data.text} {'\n'}{' '}
+        </Text>
         <Text style={styles.author}> {data.author} </Text>
       </ScrollView>
     </SafeAreaView>
--- components/Saints.tsx
+++ components/Saints.tsx
@@ -1,96 +1,106 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import {
-    View,
-    Text,
-    StatusBar,
-    StyleSheet,
-    FlatList,
-    Platform,
-    SafeAreaView,
-    Button,
-    Linking,
-} from 'react-native';
-import Database from "../db/db";
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  FlatList,
+  Platform,
+  SafeAreaView,
+  Button,
+  Linking,
+} from 'react-native'
+import Database from '../db/db'
 
-const db = new Database();
+const db = new Database()
 //import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
 
 const styles = StyleSheet.create({
-    noMeLaContainer: {
-        flex: 1,
-        textAlign: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-        paddingHorizontal: 20,
-        paddingBottom: 15
-    },
-    saint: {
-        textAlign: 'left',
-        justifyContent: 'flex-start',
-    },
-    info: {
-        alignContent: 'center',
-        justifyContent: 'center'
-    }
-});
+  noMeLaContainer: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingHorizontal: 20,
+    paddingBottom: 15,
+    backgroundColor: '#024959',
+  },
+  saint: {
+    textAlign: 'left',
+    justifyContent: 'flex-start',
+  },
+  info: {
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+})
 
 function getTodaysVaticanLink(): string {
-    const today = new Date();
-    let month = today.getMonth() + 1;
-    let day = today.getDate();
-    let monthStr = month.toString();
-    let dayStr = day.toString();
-    if (month < 10)
-        monthStr = '0' + monthStr;
-    if (day < 10)
-        dayStr = '0' + dayStr;
-    const link = 'https://www.vaticannews.va/es/santos/' + monthStr +
-        '/' + dayStr + '.html';
-    return link;
+  const today = new Date()
+  let month = today.getMonth() + 1
+  let day = today.getDate()
+  let monthStr = month.toString()
+  let dayStr = day.toString()
+  if (month < 10) monthStr = '0' + monthStr
+  if (day < 10) dayStr = '0' + dayStr
+  const link =
+    'https://www.vaticannews.va/es/santos/' + monthStr + '/' + dayStr + '.html'
+  return link
 }
 
-
 export default function DailySaint() {
-    const [dailySaints, setdailySaints] = useState([]);
-    const [isThereAnyInfo, setisThereAnyInfo] = useState(true);
+  const [dailySaints, setdailySaints] = useState([])
+  const [isThereAnyInfo, setisThereAnyInfo] = useState(true)
 
-    useEffect(() => {
-        const dailySaints = db.getDailySaints()
-        typeof dailySaints !== 'undefined' && dailySaints.length === 0 ? setisThereAnyInfo(false) : setdailySaints(dailySaints)
-    }, []);
+  useEffect(() => {
+    const dailySaints = db.getDailySaints()
+    typeof dailySaints !== 'undefined' && dailySaints.length === 0
+      ? setisThereAnyInfo(false)
+      : setdailySaints(dailySaints)
+  }, [])
 
-    const SaintView = ({ _saintObj }) => (
-        <View>
-            <View style={styles.saint}>
-                <Text style={{ fontSize: 25 }}> {_saintObj.saint} </Text>
-            </View>
+  const SaintView = ({ _saintObj }) => (
+    <View>
+      <View style={styles.saint}>
+        <Text style={{ fontSize: 25, color: 'white' }}>
+          {' '}
+          {_saintObj.saint}{' '}
+        </Text>
+      </View>
 
-            <View style={styles.info}>
-                <Text style={{ fontSize: 20 }} >{_saintObj.info}{"\n"} </Text>
-            </View>
-        </View>
-    )
+      <View style={styles.info}>
+        <Text style={{ fontSize: 20, color: 'white' }}>
+          {_saintObj.info}
+          {'\n'}{' '}
+        </Text>
+      </View>
+    </View>
+  )
 
-    return isThereAnyInfo ?
-        (
-            <SafeAreaView style={styles.noMeLaContainer}>
-                <FlatList
-                    data={dailySaints}
-                    renderItem={({ item }) => <SaintView _saintObj={item} />}
-                    keyExtractor={(saint) => saint.saint}
-                    ListHeaderComponent={<SaintView _saintObj={dailySaints} />}
-                    ListFooterComponent={<Button title='Para leer mas sobre los santos del dia ingresa aquí'
-                        onPress={_ => Linking.openURL(getTodaysVaticanLink())}
-                    />}
-                />
-            </SafeAreaView>
-        ) :
-        (
-            <SafeAreaView style={styles.noMeLaContainer}>
-                <Text style={{ fontSize: 30, textAlign: 'center' }} > No hay nigun santo del dia para esta fecha :( </Text>
-            </SafeAreaView>
-        )
+  return isThereAnyInfo ? (
+    <SafeAreaView style={styles.noMeLaContainer}>
+      <FlatList
+        data={dailySaints}
+        renderItem={({ item }) => <SaintView _saintObj={item} />}
+        keyExtractor={saint => saint.saint}
+        ListHeaderComponent={<SaintView _saintObj={dailySaints} />}
+        ListFooterComponent={
+          <Button
+            title='Para leer mas sobre los santos del dia ingresa aquí'
+            onPress={_ => Linking.openURL(getTodaysVaticanLink())}
+          />
+        }
+      />
+    </SafeAreaView>
+  ) : (
+    <SafeAreaView style={styles.noMeLaContainer}>
+      <Text style={{ fontSize: 30, textAlign: 'center' }}>
+        {' '}
+        No hay nigun santo del dia para esta fecha :({' '}
+      </Text>
+    </SafeAreaView>
+  )
 }
