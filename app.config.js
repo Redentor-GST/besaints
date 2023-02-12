d28b62c79c877358dde2a0862021cdfd0e2c2698
jramost <jramos@seenka.com>
2023-02-12 13:09:26 -0300

    :broom:

---
+++ app.config.js
@@ -0,0 +1,41 @@
+export default {
+  expo: {
+    name: 'Be Saints',
+    slug: 'besaints',
+    version: '0.0.2.1',
+    orientation: 'portrait',
+    icon: './assets/applogo.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      infoPlist: {},
+      bundleIdentifier: 'be.saints.client',
+    },
+    notification: {
+      icon: './assets/notiflogo.png',
+    },
+    android: {
+      googleServicesFile: process.env.GOOGLE_SERVICES_FILE,
+      package: 'be.saints.client',
+      versionCode: 8,
+      adaptiveIcon: {
+        backgroundImage: './assets/applogo.png',
+        foregroundImage: './assets/applogo.png',
+        backgroundColor: '#ffffff00',
+      },
+      permissions: ['NOTIFICATIONS'],
+    },
+    extra: {
+      eas: {
+        projectId: '60078d31-0462-4957-90c7-33c2afdc19c7',
+      },
+    },
+  },
+}
