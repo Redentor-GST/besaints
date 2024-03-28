482aa33e138fbe2b9006b3b9ad306ec51dd1ee46
Julian Ramos <jramostod@gmail.com>
2024-03-28 12:33:26 -0300

    changes owner

--- utils/utils.ts
+++ utils/utils.ts
@@ -6,6 +6,16 @@ export const timeTriggerAsString = (hours: number, minutes: number) => {
     return `${h}:${m}`
 }
 
+export function getDateStr(date: Date, incrementMonth: boolean) {
+    let month: any = incrementMonth ? date.getMonth() + 1 : date.getMonth()
+    let day: any = date.getDate()
+
+    if (day < 10) day = `0${day}`
+    if (month < 10) month = `0${month}`
+
+    return `${month}-${day}`
+}
+
 export const sharePhrase = async (phrase: string) => {
     await Share.share({
         message:
